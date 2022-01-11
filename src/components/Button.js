1b59d39041b4c3ba8a277366c298a48cf648a287
Gulam Anas <g.anas786@gmail.com>
2022-01-11 22:24:13 +0530

    finished working on navbar

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './Button.css'
 
 const STYLES = ['btn_primary', 'btn_outline'];
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import './Nav.css'
+import { Button } from './Button';
 import { Link } from 'react-router-dom'
 
 function Nav() {
