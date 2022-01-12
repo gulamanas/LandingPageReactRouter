da2c7f4dbbbd016af53f88f19fa1affadca29420
Gulam Anas <g.anas786@gmail.com>
2022-01-12 22:04:55 +0530

    created hero section component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import './Hero.css'
+import '../App.css'
+import { Button } from './Button'
 
 function Hero() {
     return (
         <div className='hero_container'>
             <video src="./video/video-2.mp4" autoPlay loop muted></video>
+            <h1>ADVENTURE AWAITS</h1>
+            <p>What are you waiting for?</p>
+            <div className="hero_btns">
+                <Button className='btns' buttonStyle='btn_outline' buttonSize='btn-large' >
+                    GET STARTED
+                </Button>
+                <Button className='btns' buttonStyle='btn_primary' buttonSize='btn-large' >
+                    WATCH TRAILER <i className="fas fa-play-circle"></i>
+                </Button>
+            </div>
         </div>
     )
 }
