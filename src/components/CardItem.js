104d1f96f7ba01ca4eca7911a3cb7447630efdc7
Gulam Anas <g.anas786@gmail.com>
2022-01-15 21:55:37 +0530

    created card component

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function CardItem() {
+
+function CardItem(props) {
     return (
         <div>
             <li className="cards_item">
-                <Link to="" className="cards_item_link">
-                    <figure className="cards_item_pic_wrap">
-                        <img src="/" alt="Travel Image" className="cards_item_img" />
+                <Link to={props.path} className="cards_item_link">
+                    <figure className="cards_item_pic_wrap" data-category={props.label}>
+                        <img src={props.src} alt="Travel Image" className="cards_item_img" />
                     </figure>
                     <div className="cards_item_info">
                         <h5 className="cards_item_text">
-
+                            {props.text}
                         </h5>
                     </div>
                 </Link>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CardItem from './CardItem'
+import './Cards.css'
 
 function Cards() {
     return (
@@ -8,7 +9,38 @@ function Cards() {
             <div className="cards_container">
                 <div className="cards_wrapper">
                     <ul className='cards_items'>
-                        <CardItem />
+                        <CardItem
+                            src='images/img-2.jpg'
+                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                            label='Luxury'
+                            path='/services'
+                        />
+                        <CardItem
+                            src='images/img-9.jpg'
+                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                            label='Adventure'
+                            path='/services'
+                        />
+                    </ul>
+                    <ul className='cards_items'>
+                        <CardItem
+                            src='images/img-9.jpg'
+                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                            label='Adventure'
+                            path='/services'
+                        />
+                        <CardItem
+                            src='images/img-9.jpg'
+                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                            label='Adventure'
+                            path='/services'
+                        />
+                        <CardItem
+                            src='images/img-9.jpg'
+                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                            label='Adventure'
+                            path='/services'
+                        />
                     </ul>
                 </div>
             </div>
